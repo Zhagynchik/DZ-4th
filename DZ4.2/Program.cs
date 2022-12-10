e6d0f6713dc41994c2849020c755018765ed700d
zhagynchik <zhagynchik@mail.ru>
2022-12-10 15:54:02 +0300

    First commit

---
+++ DZ4.2/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+82 -> 10
+9012 -> 12   */
+
+
+Console.WriteLine("Введите число ");
+string number1Str = Console.ReadLine();
+int number1 = Convert.ToInt32(number1Str);
+
+int summa = 0;
+
+while (number1 > 0)
+{
+    summa += number1 % 10;
+    number1 /= 10;
+}
+Console.WriteLine(summa);
+
+
